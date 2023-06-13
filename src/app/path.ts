535fa89cb825b2a00a8ebbfa9043a8d7f7add05b
Igor Shargin <igor20513@gmail.com>
2023-06-13 20:36:15 +0500

    fix: refactor path FORGOT_PASSWORD

--- src/app/path.ts
+++ src/app/path.ts
@@ -2,7 +2,7 @@ export const PATH = {
     HOME: '/',
     LOGIN: '/auth/login',
     REGISTRATION: '/auth/registration',
-    FORGOT_PASSWORD: '/forgot-password',
+    FORGOT_PASSWORD: '/auth/forgot-password',
     EXPIRED_LINK: '/auth/email-resending',
     REGISTRATION_CONFIRMED: '/auth/registrationConfirmed',
     CREATE_NEW_PASSWORD: '/create-new-password/:token',
