8eb682313d03621401d4ca6897ef7dc9f1504e0a
nazarPtyt <ockap90@libero.it>
2023-06-14 20:54:36 +0200

    fix: theme color for NavIconWrapper

--- src/common/components/Aside/Aside.tsx
+++ src/common/components/Aside/Aside.tsx
@@ -2,13 +2,14 @@
 import React from 'react'
 import {AsideWrapper} from 'common/components/Aside/styled'
 import {NavLink} from 'common/components/NavLink/NavLink'
-import HomeWhite from './../../assets/icons/homeWhite.svg'
+import {HomeWhite} from 'common/assets/icons/homeWhite'
+// import HomeWhite from 'common/assets/icons/homeWhite'
 
 export const Aside = () => {
     return (
         <AsideWrapper>
             <nav>
-                <NavLink href={'/home'} name={'Home'} icon={<HomeWhite />} />
+                <NavLink href={'/profile/home'} name={'Home'} icon={<HomeWhite />} />
                 <NavLink href={'/profile/messenger'} name={'Messenger'} icon={<HomeWhite />} />
                 <NavLink href={'/profile/statistics'} name={'Statistics'} icon={<HomeWhite />} />
             </nav>
