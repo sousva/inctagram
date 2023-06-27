3b6b7360e4c72dad696a35c01dc80b973507fa80
yaromahob <yaromahob@gmail.com>
2023-06-27 22:52:42 +0300

    fix: datePicker - choose year/month with select

--- src/common/components/DatePicker/DatePicker.tsx
+++ src/common/components/DatePicker/DatePicker.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import {CustomDatePickerWrapper} from 'common/components/DatePicker/styled'
+import {CustomDatePickerWrapper, DatePickerHeader} from 'common/components/DatePicker/styled'
 import {Control, Controller} from 'react-hook-form'
 import {IFormInput} from 'common/components/GeneralInformation/GeneralInformationForm/GeneralInformationForm'
+import range from 'lodash/range'
+import {getMonth, getYear} from 'date-fns/fp'
+import {months} from './constants'
 
 type DatePickerPropsType = {
     control: Control<IFormInput, {dateOfBirthday: Date}>
 }
 
 export const CustomDatePicker = React.forwardRef<DatePicker, DatePickerPropsType>((props, ref) => {
+    const years = range(1923, getYear(new Date()) + 1, 1)
+
+    const yearsList = years.map(option => (
+        <option key={option} value={option}>
+            {option}
+        </option>
+    ))
+
+    const monthsList = months.map(option => (
+        <option key={option} value={option}>
+            {option}
+        </option>
+    ))
+
     return (
         <CustomDatePickerWrapper>
             Date of Birthday <br />
@@ -22,6 +39,40 @@ export const CustomDatePicker = React.forwardRef<DatePicker, DatePickerPropsType
                         dateFormat={'dd.MM.yyyy'}
                         selected={field.value}
                         onChange={date => field.onChange(date)}
+                        renderCustomHeader={({
+                            date,
+                            changeYear,
+                            changeMonth,
+                            decreaseMonth,
+                            increaseMonth,
+                            prevMonthButtonDisabled,
+                            nextMonthButtonDisabled,
+                        }) => {
+                            return (
+                                <DatePickerHeader>
+                                    <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+                                        {'<'}
+                                    </button>
+                                    <select
+                                        value={getYear(date)}
+                                        onChange={({target: {value}}) => changeYear(Number(value))}
+                                    >
+                                        {yearsList}
+                                    </select>
+
+                                    <select
+                                        value={months[getMonth(date)]}
+                                        onChange={({target: {value}}) => changeMonth(months.indexOf(value))}
+                                    >
+                                        {monthsList}
+                                    </select>
+
+                                    <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+                                        {'>'}
+                                    </button>
+                                </DatePickerHeader>
+                            )
+                        }}
                     />
                 )}
             />
