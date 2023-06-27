832fd6739ac2e811386cc6eec603c741f646084e
nazarPtyt <ockap90@libero.it>
2023-06-27 22:29:45 +0200

    fix: bug with middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,2 @@
-// import {NextResponse} from 'next/server'
-// import type {NextRequest} from 'next/server'
-// import {PATH} from 'app/path'
-//
-// export function middleware(request: NextRequest) {
-//     if (request.nextUrl.pathname.startsWith('/')) {
-//         return NextResponse.redirect(new URL(PATH.LOGIN, request.url))
-//     }
-//     // if (request.nextUrl.pathname.startsWith('/dashboard')) {
-//     //     return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-//     // }
-// }
 export {default} from 'next-auth/middleware'
-export const config = {matcher: ['/profile']}
+export const config = {matcher: ['/']}
