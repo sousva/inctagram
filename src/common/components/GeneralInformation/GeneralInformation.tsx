866d4ce290286b2c75fc006ee2792df68a1ed209
yaromahob <yaromahob@gmail.com>
2023-07-03 16:09:42 +0300

    fix: check if avatar of array is not empty

--- src/common/components/GeneralInformation/GeneralInformation.tsx
+++ src/common/components/GeneralInformation/GeneralInformation.tsx
@@ -5,7 +5,7 @@ import {Avatar} from 'common/components/Avatar/Avatar'
 import {UserProfile} from '../../../pages/profile/profile-settings'
 
 export const GeneralInformation: FC<{data: UserProfile}> = ({data}) => {
-    const avatar = data.avatars[0].url
+    const avatar = data.avatars.length !== 0 ? data.avatars[0].url : ''
     return (
         <GeneralInformationWrapper>
             <Avatar avatar={avatar} />
